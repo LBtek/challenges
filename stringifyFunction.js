3fcd3bd238816ff74072d9e82742be0e8a7c5b5b
Luan Rafael <luanrafaelberto@gmail.com>
2023-01-20 15:36:23 -0400

    improved stringify function

--- stringifyFunction.js
+++ stringifyFunction.js
@@ -1,92 +1,147 @@
-export function stringify(item) {
+function prepare( item ) {
+
   let response
-  if (item instanceof Map) 
-    response = prepareMap(item)
-  else if (item instanceof Array) 
-    response = prepareArray(item)
-  else if (item instanceof Set) 
-    response = prepareSet(item)
-  else if (item instanceof Object) 
-    response = prepareObject(item)
-  else response = item
+
+  if ( item instanceof Map )
+     response = prepareMap(item)
+
+  else if ( item instanceof Array )
+     response = prepareArray(item)
+
+  else if ( item instanceof Set )
+     response = prepareSet(item)
+
+  else if ( item instanceof Object )
+     
+     if ( item instanceof String )
+        response = item
+  
+     else if ( item instanceof Function )
+        response = typeof item.toString === 'function' 
+           && typeof item.toString() === 'string' 
+              ? item.toString() 
+              : Object.prototype.toString.call(item)
+
+     else 
+        response = prepareObject(item)
+
+  else
+     response = item
+
+
+  return response
+
+}
+
+
+export function stringify( item ) {
+
+  const response = prepare(item)
+
+  ws = new WeakSet()
+
 
   return JSON.stringify(response)
+
 }
 
-function prepareObject(object) {
+
+let ws = new WeakSet()
+
+
+function prepareObject( object ) {
+
+  if ( ws.has(object) )
+     return Object.prototype.toString.call(object)
+
+  ws.add(object)
+
+  let array = Object.entries(object)
+
   let obj = {}
-  let arr = Object.entries(object)
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i][1] instanceof Map) {
-      obj[arr[i][0]] = prepareMap(arr[i][1])
-    } else if (arr[i][1] instanceof Array) {
-      obj[arr[i][0]] = prepareArray(arr[i][1])
-    } else if (arr[i][1] instanceof Set) {
-      obj[arr[i][0]] = prepareSet(arr[i][1])
-    } else if (arr[i][1] instanceof Object) {
-      obj[arr[i][0]] = prepareObject(arr[i][1])
-    } else {
-      obj[arr[i][0]] = arr[i][1]
-    }
+
+  for ( let i = 0; i < array.length; i++ ) {
+
+     const key = array[i][0]
+
+     obj[key] = prepare(array[i][1])
+
   }
+
+
   return obj
+
 }
 
-function prepareArray(array) {
-  let arr = new Array(array.length)
-  array.forEach((e, id) => {
-    if (e instanceof Map) {
-      arr[id] = prepareMap(e)
-    } else if (e instanceof Array) {
-      arr[id] = prepareArray(e)
-    } else if (e instanceof Set) {
-      arr[id] = prepareSet(e)
-    } else if (e instanceof Object) {
-      arr[id] = prepareObject(e)
-    } else arr[id] = e
+
+function prepareArray( array ) {
+
+  if ( ws.has(array) )
+     return Object.prototype.toString.call(array)
+
+  ws.add(array)
+
+  let newArray = new Array(array.length)
+
+  array.forEach(( item, i ) => {
+
+     newArray[i] = prepare(item)
+
   })
-  return arr
+
+
+  return newArray
+
 }
 
-function prepareSet(setObj) {
-  let arr = [...setObj]
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] instanceof Map) {
-      arr[i] = prepareMap(arr[i])
-    } else if (arr[i] instanceof Array) {
-      arr[i] = prepareArray(arr[i])
-    } else if (arr[i] instanceof Set) {
-      arr[i] = prepareSet(arr[i])
-    } else if (arr[i] instanceof Object) {
-      arr[i] = prepareObject(arr[i])
-    }
+function prepareSet( setObj ) {
+
+  if ( ws.has(setObj) )
+     return Object.prototype.toString.call(setObj)
+
+  ws.add(setObj)
+
+  let array = [...setObj]
+
+  for ( let i = 0; i < array.length; i++ ) {
+
+     array[i] = prepare(array[i])
+
   }
+
+  if ( !array.length ) 
+     return Object.prototype.toString.call(setObj)
+
+
+  return array
+
 }
 
-function prepareMap(map) {
-  let arr = [...map]
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i][0] instanceof Map) {
-      arr[i][0] = prepareMap(arr[i][0])
-    } else if (arr[i][0] instanceof Array) {
-      arr[i][0] = prepareArray(arr[i][0])
-    } else if (arr[i][0] instanceof Set) {
-      arr[i][0] = prepareSet(arr[i][0])
-    } else if (arr[i][0] instanceof Object) {
-      arr[i][0] = prepareObject(arr[i][0])
-    }
+function prepareMap( map ) {
 
-    if (arr[i][1] instanceof Map) {
-      arr[i][1] = prepareMap(arr[i][1])
-    } else if (arr[i][1] instanceof Array) {
-      arr[i][1] = prepareArray(arr[i][1])
-    } else if (arr[i][1] instanceof Set) {
-      arr[i][1] = prepareSet(arr[i][1])
-    } else if (arr[i][1] instanceof Object) {
-      arr[i][1] = prepareObject(arr[i][1])
-    }
+  if ( ws.has(map) )
+     return Object.prototype.toString.call(map)
+
+  ws.add(map)
+
+  let array = [...map]
+
+  for ( let i = 0; i < array.length; i++ ) {
+
+     array[i][0] = prepare(array[i][0])
+
+     array[i][1] = prepare(array[i][1])
+
   }
-  return arr
+
+  if ( !array.length ) 
+     return Object.prototype.toString.call(map)
+  
+
+  return array
+
 }
+
+module.exports = stringify
